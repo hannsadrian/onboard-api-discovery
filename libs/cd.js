5e5b7976324fa02248f082477bd890f914507813
Adrian <hannsadrian@gmail.com>
2021-08-29 16:10:29 +0200

    Update output from cd wifi check

--- libs/cd.js
+++ libs/cd.js
@@ -1,32 +1,28 @@
-const {
-  fetchTrainStatus,
-  fetchWifiStatus,
-  fetchWifiConnectivity,
-} = require("cd-wifi-client");
+const { fetchTrainStatus, fetchWifiStatus, fetchWifiConnectivity } = require('cd-wifi-client')
 
 const info = {
-  system: "Czech Railways",
-  client: "cd-wifi-client",
-};
+  system: 'Czech Railways',
+  client: 'cd-wifi-client'
+}
 
 const isAvailable = async () => {
   try {
-    const payload = {};
-    payload.status = await fetchTrainStatus();
-    payload.status = await fetchWifiStatus();
-    payload.status = await fetchWifiConnectivity();
+    const payload = {}
+    payload.trainStatus = await fetchTrainStatus()
+    payload.wifiStatus = await fetchWifiStatus()
+    payload.wifiConnectivity = await fetchWifiConnectivity()
     return {
       available: true,
       info: info,
-      payload: payload,
-    };
+      payload: payload
+    }
   } catch (err) {
     return {
       available: false,
       info: info,
-      payload: null,
-    };
+      payload: null
+    }
   }
-};
+}
 
-module.exports = isAvailable;
+module.exports = isAvailable
